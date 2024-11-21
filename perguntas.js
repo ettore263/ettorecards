f8950c7397e4ce52104c517e87fa6d1a157eed10
ettore263 <fernandesettoreaa@gmail.com>
2024-11-21 14:30:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'Bankai',
+    'Quantos anos leva para se poder usar a bankai em bleach?',
+    'Cerca de dez anos de treinamento individual para ser executado.'
+)
+
+criaCartao(
+    'O Rei',
+    'Quem é o rei dos Quincys?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
